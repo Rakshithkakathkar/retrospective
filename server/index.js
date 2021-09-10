79b02ab87020ec82928239b0a7f161e1774a112d
Rakshith kakathkar <rakshithkakathkar@gmail.com>
2021-09-10 20:30:42 +0530

    added connection to mongo

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -15,4 +15,7 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`)))
+    .catch((error) => console.log(error.message));
